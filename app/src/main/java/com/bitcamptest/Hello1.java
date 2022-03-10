f84c9746be3ed6ae2c7dadf75c805c6f19c2e6d5
poohbearr <syb369@naver.com>
2022-03-10 17:47:39 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/omh1004/bitcamp-test.git into
    main

--- app/src/main/java/com/bitcamptest/Hello1.java
+++ app/src/main/java/com/bitcamptest/Hello1.java
@@ -4,5 +4,10 @@ public class Hello1 {
 
   public static void main(String[] args) {
     System.out.println("팀원1입니다.");
+    haha();
+  }
+
+  static void haha() {
+    System.out.println("hahahahaha");
   }
 }
