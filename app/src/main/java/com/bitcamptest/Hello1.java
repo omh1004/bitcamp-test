eea48ba5fafeb5c90e544f1e4f82ec8d89107b1f
eomjinyoung <jinyoung.eom@gmail.com>
2022-03-10 17:47:42 +0900

    백업3

--- app/src/main/java/com/bitcamptest/Hello1.java
+++ app/src/main/java/com/bitcamptest/Hello1.java
@@ -4,5 +4,10 @@ public class Hello1 {
 
   public static void main(String[] args) {
     System.out.println("팀원1입니다.");
+    m1();
+  }
+
+  static void m1() {
+    System.out.println("강사입니다!");
   }
 }
